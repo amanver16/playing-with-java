f6a86203ca8a61b841a819ff370cc5238cc085d3
Aman Verma <33910746+amanver16@users.noreply.github.com>
2018-04-03 14:50:28 +0530

    JUnit Demo using Maven
    This project demonstrates on how you can unit test your service classes and methods i.e. business logic.

---
+++ JunitDemo/src/test/java/com/stpl/trainee/aman/junitdemo/circlerectangle/CircleServiceTest.java
@@ -0,0 +1,44 @@
+package com.stpl.trainee.aman.junitdemo.circlerectangle;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.stpl.trainee.aman.junitdemo.service.circlerectangle.CircleService;
+
+/* ################### */
+/* Author : AMAN VERMA */
+/* ################### */
+
+public class CircleServiceTest {
+
+	private CircleService circle;
+
+	@Before
+	public void initial() {
+		circle = new CircleService();
+	}
+
+	// Circle Area Service Test Method
+	@Test
+	public void testArea() {
+
+		circle.setRadius(3.00f);
+		assertEquals(28.26f, circle.calculateCircleAreaService(), 00.00);
+	}
+
+	// Circle Circumference Service Test Method
+	@Test
+	public void testCircumference() {
+
+		circle.setRadius(3.0f);
+		assertEquals(18.84f, circle.calculateCircleCircumferenceService(), 00.00);
+	}
+
+	@After
+	public void end() {
+		circle = null;
+	}
+}
