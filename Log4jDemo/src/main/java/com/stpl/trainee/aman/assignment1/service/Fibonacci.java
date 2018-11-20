9c17a05df3c590192697ace90579bfb0011e9fe9
Aman Verma <aman.verma@sysbiz.org>
2018-11-20 17:38:57 +0530

    Added Log4j Demo Project

---
+++ Log4jDemo/src/main/java/com/stpl/trainee/aman/assignment1/service/Fibonacci.java
@@ -0,0 +1,41 @@
+package com.stpl.trainee.aman.assignment1.service;
+
+/* ################### */
+/* Author : AMAN VERMA */
+/* ################### */
+
+public class Fibonacci {
+
+	private long parameter;
+
+	public Fibonacci(int parameter) {
+		this.parameter = parameter;
+	}
+
+	//Fibonacci Series Generator Method
+	public long[] generateFibonacciSeries() {
+
+		long first = 0;
+		long second = 1;
+		long sum = 0;
+		int i = 3;
+		long[] fibonacciSeriesArray = new long[50];
+		int fibonacciSeriesArrayIndex = 0;
+
+		fibonacciSeriesArray[fibonacciSeriesArrayIndex] = first;
+		fibonacciSeriesArray[fibonacciSeriesArrayIndex + 1] = second;
+		fibonacciSeriesArrayIndex = 2;
+
+		while (i <= parameter) {
+
+			sum = first + second;
+			fibonacciSeriesArray[fibonacciSeriesArrayIndex] = sum;
+			first = second;
+			second = sum;
+			i++;
+			fibonacciSeriesArrayIndex++;
+		}
+
+		return fibonacciSeriesArray;
+	}
+}
